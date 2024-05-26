f920486bc3e33c93417afa64e455d22d9e076c1e
Roshan-DSBA <130641151+Roshan-DSBA@users.noreply.github.com>
2024-05-26 03:34:02 +0200

    rerag update

---
+++ Project/prompts.py
@@ -0,0 +1,56 @@
+system_prompt_template = """
+<s>[INST]You are a helpful chatbot.
+You will answer questions about administrative processes related to universities and student life. 
+You are given a question: {query}. Provide a clear and structured answer in a markdown format based on the passages provided, the guidelines and example.
+You always respond in the same language as of the question. 
+[/INST]
+"""
+
+system_prompt_reformulate =  """ 
+
+<s>[INST]You are a question rephraser with translation. In the context of student queries, rephrase the question without losing keywords into french and english
+The output structure should strictly be.
+
+English: 
+French:
+[/INST]
+"""
+
+custom_answer_prompt_template = """ 
+<s>[INST] Follow the guidelines:
+
+- Always follow the structure in Examples
+- If the passages has attachment links, add them under sources
+- If the passages has last modified data, use that in the answer.
+- You have to answer in the same language as of the question.
+- If the passages are not relevant to the question, say "That's an interesting question! However,I can not help you.”
+- If there are urls as part of the context, share the url at the end of the answer along with a the description of what it contains.
+- Sources link should ALWAYS be hyperlinked in markdown structure [Document name](link)
+
+Using the context below:
+
+-----------------------
+Passages:
+{context}
+-----------------------
+Question: {query}
+
+
+Examples:
+
+Question: ""
+
+Answer: 
+
+
+Sources:
+1. [Document Name] (url)
+2. [Document Name] (url)
+
+Attachments:
+1. url
+2. url
+[/INST]
+
+"""
+
--- Project/retriever.py
+++ Project/retriever.py
@@ -3,12 +3,69 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 from langchain_mistralai.embeddings import MistralAIEmbeddings
+from langchain_mistralai import ChatMistralAI
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+import re
+
+
 
 api_key = os.getenv('MISTRAL_API')
+groq_api = os.getenv('GROQ_API')
 
 embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key= api_key)
 
 
+custom_answer_prompt_template = """ 
+
+<s>[INST]You are a question rephraser with translation. In the context of student queries, rephrase the question: {query} without losing keywords into french and english
+The output structure should strictly be:
+
+'English: 
+French:'
+
+
+[/INST]
+"""
+answer_prompt = PromptTemplate(template=custom_answer_prompt_template, input_variables=['context', 'query'])
+
+
+chat = ChatGroq(temperature=0, groq_api_key = groq_api, model_name="Mixtral-8x7b-32768")
+
+# Define the regular expressions for English and French questions
+english_pattern = r'English:\s*(.*?)(?=French:|$)'
+french_pattern = r'French:\s*(.*)'
+
+def reform(query, llm):
+    formatted_prompt = answer_prompt.format(query=query)
+    text = llm.invoke(formatted_prompt).content
+    
+    english_match = re.search(english_pattern, text, re.DOTALL)
+    english_question = english_match.group(1).strip() if english_match else None
+
+    # Extract French question
+    french_match = re.search(french_pattern, text, re.DOTALL)
+    french_question = french_match.group(1).strip() if french_match else None
+    
+    return english_question, french_question
+
+def rerag(query, collection_name, cursor, top_k=5):
+    
+    english_question, french_question = reform(query, chat)
+    
+    english_results = get_common_results(english_question, collection_name, cursor, top_k=5)
+    
+    french_results = get_common_results(french_question, collection_name, cursor, top_k = 5)
+    
+    # Find common tuples based on id
+    content_ids = {row[0] for row in english_results}
+    common_results = [row for row in french_results if row[0] in content_ids]
+    
+    return common_results if common_results else english_results
+    
+    
+    
+    
 def query_neon(query, collection_name, cursor, vector_name="title_vector", top_k=5):
     try:
         # Create an embedding vector from the user query
